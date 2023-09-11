6b3f4ce89248f0e76c02f499357a4afaea507774
michaelmftran <michaeltranb@outlook.com>
2023-09-11 17:10:07 -0500

    first commit
    commited python html and css files

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, render_template, request, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    mode = request.cookies.get('mode', 'light')
+    return render_template('home.html', mode=mode)
+
+@app.route('/about')
+def about():
+    mode = request.cookies.get('mode', 'light')
+    return render_template('about.html', mode=mode)
+
+@app.route('/skills')
+def skills():
+    mode = request.cookies.get('mode', 'light')
+    return render_template('skills.html', mode=mode)
+
+@app.route('/projects')
+def projects():
+    mode = request.cookies.get('mode', 'light')
+    return render_template('projects.html', mode=mode)
+
+@app.route('/contact')
+def contact():
+    mode = request.cookies.get('mode', 'light')
+    return render_template('contact.html', mode=mode)
+
+@app.route('/set_mode', methods=['POST'])
+def set_mode():
+    mode = request.form.get('mode')
+    res = make_response("Mode set")
+    res.set_cookie('mode', mode, max_age=60*60*24*365*2)  # Set cookie for 2 years
+    return res
+
+if __name__ == '__main__':
+    app.run(debug=True)
